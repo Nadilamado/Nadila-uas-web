4a9d9d2b14da43872d279dd232e5689108cb0150
Nadilamado <nadilsmado@gmail.com>
2023-02-15 19:27:42 +0900

    Nadila-uas-web

---
+++ public/content/galeri.php
@@ -0,0 +1,25 @@
+    <header class="container bg-warning  text-white">
+        <div class="row">
+            <div class="col-12 py-2 text-center">
+                <h2 class="display-6 ">Galleri </h2>
+            </div>
+        </div>
+    </header>
+<main class="container border p-md-5 p-2">
+        <div class="row g-2 g-md-4">
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="nadil.jpg" alt="Nadila">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="nadil1.jpg" alt="Nadila">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="nadil2.jpg" alt="Nadila">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="nadil3.jpg" alt="Nadila">
+            </div>
+            
+        </div>
+    </main>
+
